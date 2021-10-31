2f5b0b0c7385d508b6cff0f5e3a2de63a8e052ac
Jyotsna <guptajyotsna2002@gmail.com>
2021-10-31 13:25:11 +0530

    background,font colour,text size fix

--- app/src/main/java/com/example/cinebuzz/MainActivity.kt
+++ app/src/main/java/com/example/cinebuzz/MainActivity.kt
@@ -1,11 +1,10 @@
 package com.example.cinebuzz
-
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.login_fragment)
+        setContentView(R.layout.activity_main)
     }
 }
