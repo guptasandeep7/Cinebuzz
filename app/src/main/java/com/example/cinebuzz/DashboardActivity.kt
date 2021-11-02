9812f22ccf850ed74f440683fbc747c5a9056090
Jyotsna <guptajyotsna2002@gmail.com>
2021-11-02 10:59:39 +0530

    navigation bar created

--- app/src/main/java/com/example/cinebuzz/DashboardActivity.kt
+++ app/src/main/java/com/example/cinebuzz/DashboardActivity.kt
@@ -2,10 +2,29 @@ package com.example.cinebuzz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.cinebuzz.auth.VerifyFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class DashboardActivity : AppCompatActivity() {
+
+
+     private lateinit var bottomNav: BottomNavigationView
+     private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
+
+      bottomNav=findViewById(R.id.bottomNav)
+        navController=findNavController(R.id.fragmentContainerView2)
+       bottomNav.setupWithNavController(navController)
+
     }
 }
