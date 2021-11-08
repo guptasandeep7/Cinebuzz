c3c8aba616a24a80011277ab66a75e00f04d8b33
Sandeep Gupta <guptasg300@gmail.com>
2021-11-08 19:50:13 +0530

    navigation drawer border curve

--- app/src/main/java/com/example/cinebuzz/retrofit/ServiceBuilder.kt
+++ app/src/main/java/com/example/cinebuzz/retrofit/ServiceBuilder.kt
@@ -6,7 +6,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 object ServiceBuilder {
 
     private val retrofit = Retrofit.Builder()
-        .baseUrl("https://90a5-2401-4900-44d6-358c-44c0-15f-c9b0-dd01.ngrok.io")
+        .baseUrl("https://b0c4-2401-4900-30c4-9da0-bdd8-963f-7cc1-4217.ngrok.io/")
         .addConverterFactory(GsonConverterFactory.create())
         .build()
 
