8f1cf93e3764128a314f8f547e429b7445624495
Sandeep Gupta <guptasg300@gmail.com>
2021-11-11 16:39:15 +0530

    error fixed

--- app/src/main/java/com/example/cinebuzz/MainActivity.kt
+++ app/src/main/java/com/example/cinebuzz/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.cinebuzz
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 import com.example.cinebuzz.auth.LoginFragment
 import com.example.cinebuzz.dashboard.HomePage_fragment
 
