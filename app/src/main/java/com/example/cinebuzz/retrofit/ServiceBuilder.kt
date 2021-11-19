140257883e4b780845479774605a26ccc59aa365
Sandeep Gupta <guptasg300@gmail.com>
2021-11-20 00:17:22 +0530

    Token added

--- app/src/main/java/com/example/cinebuzz/retrofit/ServiceBuilder.kt
+++ app/src/main/java/com/example/cinebuzz/retrofit/ServiceBuilder.kt
@@ -1,6 +1,8 @@
 package com.example.cinebuzz.retrofit
 
 import com.example.cinebuzz.SplashScreen.Companion.BASEURL
+import com.example.cinebuzz.SplashScreen.Companion.TOKEN
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -9,6 +11,10 @@ object ServiceBuilder {
     private val retrofit = Retrofit.Builder()
         .baseUrl(BASEURL)
         .addConverterFactory(GsonConverterFactory.create())
+        .client(OkHttpClient.Builder().addInterceptor { chain ->
+            val request = chain.request().newBuilder().addHeader("Authorization", "Bearer ${TOKEN}").build()
+            chain.proceed(request)
+        }.build())
         .build()
 
     fun buildService():ApiInterface{
