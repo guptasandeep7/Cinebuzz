5919719a00148fa6931467c214da1fec1095648c
Sandeep Gupta <guptasg300@gmail.com>
2021-11-20 07:16:17 +0530

    Rate and Review integrated

--- app/src/main/java/com/example/cinebuzz/dashboard/profile/ReviewDataItem.kt
+++ app/src/main/java/com/example/cinebuzz/dashboard/profile/ReviewDataItem.kt
@@ -1,3 +1,3 @@
 package com.example.cinebuzz.dashboard.profile
 
-data class ReviewDataItem(val name:String, val dpUrl:String, val rating:Float, val reviewText : String)
+data class ReviewDataItem(val _id:String?=null,val name:String?=null, val dpUrl:String?=null, val rating:Float?=null, val reviewText : String?=null)
